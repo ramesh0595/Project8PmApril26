c2123984249fc0ed060a807b2b9b9c016604d95c
ramesh0595 <rameshchinnu509@gmail.com>
2026-04-07 20:26:13 +0530

    kumar code

--- src/test/java/org/sample/SampleClass.java
+++ src/test/java/org/sample/SampleClass.java
@@ -6,8 +6,9 @@ public class SampleClass {
 	
 	@Test
 	public void sampleTest() {
-		// TODO Auto-generated method stub
-
+       System.out.println("Test 4");
+       System.out.println("Test 5");
+       System.out.println("Test 6");
 	}
 	
 
